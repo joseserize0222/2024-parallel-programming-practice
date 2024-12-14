3486c2b35f99ff1b729e913263fa8740046bea1f
joseserize0222 <joseserize0222@gmail.com>
2024-12-14 02:54:23 +0100

    problem 3 solution

--- problem_2/sample_solution.cpp
+++ problem_2/sample_solution.cpp
@@ -3,7 +3,7 @@
 #include <cstdint>
 #include <algorithm>
 #include <iomanip>
-
+#include <future>
 
 std::vector<size_t> read_array() {
     size_t length, a, b, p;
@@ -19,12 +19,62 @@ std::vector<size_t> read_array() {
 
 int main() {
     auto array = read_array();
-    std::sort(array.begin(), array.end());
-
+    std::vector<std::future<void>> futures;
     size_t k;
     std::cin >> k;
-    for (size_t i = k - 1; i < array.size(); i += k) {
-        std::cout << array[i] << ' ';
+    
+    size_t number_buckets = 0;
+    
+    for (size_t i = 0; i < array.size(); i += k) { 
+        number_buckets++;
+        
+        futures.emplace_back(std::async(std::launch::async, [i, &array, k]() {
+            size_t s = i;
+            size_t e = std::min(s + k, array.size());
+            auto start = array.begin() + s;
+            auto end = array.begin() + e;
+            sort(start, end);
+        }));
+    }
+
+    std::vector<int> answers;
+    
+    for (auto& future : futures) {
+        future.get(); 
+    }
+
+    std::vector<int> starts(number_buckets);
+    std::vector<int> ends(number_buckets);
+
+    for (int i = 0; i < number_buckets;i++) {
+        starts[i] = i * k;
+        ends[i] = std::min( (i + 1)*k, array.size() );
+    }
+
+    // K-WAY
+    
+    std::priority_queue<std::pair<int, int>> q;
+
+    for (int i = 0; i < number_buckets;i++) {
+        q.push({-array[starts[i]++], i});
+    }
+
+    int counter = 0;
+
+    while (!q.empty()) {
+        auto [elem, id] = q.top();
+        q.pop();
+        if (starts[id] < ends[id]) {
+            q.push({-array[starts[id]++], id});
+        }
+        counter++;
+        if (counter % k == 0) {
+            answers.push_back(-elem);
+        }
+    }
+
+    for (auto ans : answers) {
+        std::cout << ans << " ";
     }
     std::cout << "\n";
 
